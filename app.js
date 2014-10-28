0a35c92f6da9b68d7de3332aed58dd6327439cca
Zack Michener <zack@zackmichener.net>
2014-10-28 07:30:35 +0000

    remove unused credentials var

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-var creds = require('./credentials');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
