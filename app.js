0fce19dad23a554d98f53627562b6ad91f06dd38
Zack Michener <zack@zackmichener.net>
2014-10-28 07:01:55 +0000

    use post router in app

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var posts = require('./routes/posts');
 
 var app = express();
 
@@ -25,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/posts', posts);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -45,6 +48,7 @@ if (app.get('env') === 'development') {
             error: err
         });
     });
+    mongoose.connect("mongodb://localhost/dev");
 }
 
 // production error handler
