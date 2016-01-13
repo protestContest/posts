ae0802d9d4b98f368f9f8efc24a3a1f7e7668fb8
Zack Michener <zack@zackmichener.net>
2016-01-12 21:42:10 -0800

    Add animation to PostRow component.
    This is really cool! Now that I finally figured out how to properly
    initialize React on the client-side, I can start adding dynamic
    functionality. The first thing I wanted to add was an iOS-like slide
    animation to the list elements of the post list, to reveal some context
    buttons. Given my previous experience with custom drag-and-drop
    javascript, it wasn't too hard to figure out.  I'm especially glad I can
    use CSS for all of the actual transitions, which I believe is
    resource-efficient.
    
    I will say that now more than ever I'm feeling the benefits of
    component-oriented development, both with React and RSCSS. Everything
    seems well-contained and predictable so far (with the exception of some
    strange hoops I had to go through to get the server's data on the
    client) which lets me focus on the interesting development rather than
    the architecture.

--- components/scripts/src/PostList.jsx
+++ components/scripts/src/PostList.jsx
@@ -1,9 +1,12 @@
 var React = require('react');
+var ReactDOM = require('react-dom');
 if (process.env.BROWSER) require('../../styles/post-list.less');
 
 module.exports = React.createClass({
 
   render: function() {
+    var posts = this.props.posts || [];
+
     var createRow = function(post) {
       var href = '/posts/' + post._id;
       return (<PostRow post={post} href={href} />);
@@ -11,7 +14,7 @@ module.exports = React.createClass({
 
     return (
       <div className="post-list">
-        {this.props.posts.map(createRow)}
+        {posts.map(createRow)}
         <NewPostRow />
       </div>
     );
@@ -20,10 +23,74 @@ module.exports = React.createClass({
 });
 
 var PostRow = React.createClass({
+  getInitialState: function() {
+    return { dragging: false, offset: 0, rel: 0, open: false };
+  },
+
+  onTouchStart: function(e) {
+    e.stopPropagation();
+    e.preventDefault();
+
+    var pageX = e.touches[0].pageX;
+
+    this.setState({
+      dragging: true,
+      offset: 0,
+      rel: pageX - ReactDOM.findDOMNode(this).getBoundingClientRect().left
+    });
+  },
+
+  onTouchEnd: function(e) {
+    e.stopPropagation();
+    e.preventDefault();
+    
+    this.setState({
+      dragging: false,
+      rel: 0
+    });
+    
+    var elem = ReactDOM.findDOMNode(this);
+
+    elem.addEventListener('transitionend', function() {
+      elem.style.transition = "none";
+    });
+    elem.style.transition = "transform 0.5s";
+
+    if ( (!this.state.open && this.state.offset > -25) ||
+         (this.state.open && this.state.offset > -75) ) {
+      elem.style.transform = "translate(0)";
+      this.setState({ open: false });
+    } else {
+      elem.style.transform = "translate(-100px)";
+      this.setState({ open: true });
+    }
+  },
+
+  onTouchMove: function(e) {
+    e.stopPropagation();
+    e.preventDefault();
+    if (!this.state.dragging) return;
+
+    var offset = e.touches[0].pageX - this.state.rel;
+    offset = Math.min(0, Math.max(-100, offset));
+
+    this.setState({
+      offset: offset
+    });
+
+    var elem = ReactDOM.findDOMNode(this);
+    elem.style.transform = "translate(" + offset + "px)";
+  },
+
+  onClick: function(e) {
+    e.stopPropagation();
+    e.preventDefault();
+  },
+
   render: function() {
     var created = this.props.post.created.toDateString();
     return (
-      <a className="postrow" href={this.props.href}>
+      <a onTouchStart={this.onTouchStart} onTouchEnd={this.onTouchEnd} onTouchMove={this.onTouchMove} onClick={this.onClick} className="postrow" href={this.props.href}>
         {this.props.post.title}<br/>
         <small className="postdate">{created}</small>
       </a>
