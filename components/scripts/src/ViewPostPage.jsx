dba058d9fdbf8937ca420e003d96169faeb7dd93
Zack Michener <zack@zackmichener.net>
2016-01-19 22:42:01 -0800

    hide header and toolbar on view post page when scrolling

--- components/scripts/src/ViewPostPage.jsx
+++ components/scripts/src/ViewPostPage.jsx
@@ -5,9 +5,59 @@ var ReactDOM = require('react-dom');
 if (process.env.BROWSER) require('../../styles/viewpost-layout.less');
 
 var ViewPostPage = module.exports = React.createClass({
+  getInitialState: function() {
+    return {
+      scrollTop: 0,
+      scrollDir: 'up'
+    };
+  },
 
   componentDidMount: function() {
-    // console.log('Viewing post page');
+    var node = ReactDOM.findDOMNode(this.refs.postText);
+    node.addEventListener('touchmove', this.onScroll);
+    node.addEventListener('touchend', this.onScroll);
+  },
+
+  onScroll: function() {
+    var node = ReactDOM.findDOMNode(this.refs.postText);
+    var deltaScroll = node.scrollTop - this.state.scrollTop;
+    var scrollThreshhold = 20;
+
+    if (deltaScroll > 0) {
+      // scrolling down
+      if (this.state.scrollDir === 'up') {
+        this.setState({scrollDir: 'down', scrollDist: 0});
+      } else if (this.state.scrollDist > scrollThreshhold) {
+        this.hideBars();
+      } else {
+        this.setState({scrollDist: this.state.scrollDist + Math.abs(deltaScroll)});
+      }
+    } else {
+      // scrolling up
+      if (this.state.scrollDir === 'down') {
+        this.setState({scrollDir: 'up', scrollDist: 0});
+      } else if (this.state.scrollDist > scrollThreshhold) {
+        this.showBars();
+      } else {
+        this.setState({scrollDist: this.state.scrollDist + Math.abs(deltaScroll)});
+      }
+    }
+
+    this.setState({scrollTop: node.scrollTop});
+  },
+
+  showBars: function() {
+    var headerNode = ReactDOM.findDOMNode(this.refs.pageHeader);
+    var toolbarNode = ReactDOM.findDOMNode(this.refs.toolBar);
+    headerNode.classList.remove('-hidden');
+    toolbarNode.classList.remove('-hidden');
+  },
+
+  hideBars: function() {
+    var headerNode = ReactDOM.findDOMNode(this.refs.pageHeader);
+    var toolbarNode = ReactDOM.findDOMNode(this.refs.toolBar);
+    headerNode.classList.add('-hidden');
+    toolbarNode.classList.add('-hidden');
   },
 
   render: function() {
@@ -15,17 +65,17 @@ var ViewPostPage = module.exports = React.createClass({
     var publishLabel = this.props.post.isPrivate ? 'Publish' : 'Unpublish';
     var publicText = this.props.post.isPrivate ? 'Private' : 'Public';
     var publicIcon = this.props.post.isPrivate ? 'fa-lock' : 'fa-globe';
-    
+
     return (
       <div id='content' className='viewpost-layout'>
-        <div className='page-header'>
+        <div ref='pageHeader' className='page-header'>
           <div className='page-title'>
             <h1 className='title'>{this.props.post.title}</h1>
             <small className='info'><i className={'fa ' + publicIcon}></i> {publicText}</small>
           </div>
         </div>
-        <div className='post-text' dangerouslySetInnerHTML={{__html: this.props.post.body}}></div>
-        <div className='tool-bar'>
+        <div ref='postText' className='post-text' dangerouslySetInnerHTML={{__html: this.props.post.body}}></div>
+        <div ref='toolBar' className='tool-bar'>
           <a className='toolbutton' href='/home'>
             <i className='fa fa-2x fa-list'></i>
             Posts
