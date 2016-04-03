40496dafc7505eba1ac86ca21068f93c6233d465
Zack Michener <zack@zackmichener.net>
2016-04-03 16:18:20 -0700

    add new webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,57 @@
-var webpack = require('webpack');
-var ET = require('extract-text-webpack-plugin');
-
 module.exports = {
+  entry: getEntrySources(['./src/script/entry.js']),
+  output: {
+    publicPath: 'http://localhost:8080/',
+    filename: 'build/bundle.js'
+  },
+  devtool: 'eval',
   module: {
+    preLoaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'source-map'
+      }
+    ],
     loaders: [
       {
         test: /\.less$/,
-        loader: ET.extract('css!less')
+        include: /src/,
+        loaders: [
+          {
+            test: /\.less$/,
+            include: /src/,
+            loaders: [
+              'style',
+              'css',
+              'less'
+            ]
+          }
+        ]
+      },
+      {
+        test: /\.(jpg|png|gif|svg)$/i,
+        loaders: [
+          'img'
+        ]
+      },
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        loaders: [
+          'react-hot',
+          'babel?presets[]=stage-0,presets[]=react,presets[]=es2015'
+        ]
       }
     ]
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        BROWSER: JSON.stringify(true)
-      }
-    }),
-    new ET('style.css')
-  ]
+  }
 };
+
+function getEntrySources(sources) {
+  if (process.env.NODE_ENV !== 'production') {
+    sources.push('webpack-dev-server/client?http://localhost:8080');
+    sources.push('webpack/hot/only-dev-server');
+  }
+
+  return sources;
+}
