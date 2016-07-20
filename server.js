114a19733e1f13dc8c01a56e6ae157ee3b1e424b
Zack Michener <zack@zackmichener.net>
2016-07-20 10:03:44 -0700

    second test of git hooks

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ var Post = require('./models/Post');
 
 var routes = require('./routes/index')(User, Subs, Post)
 
-var app = express();
+var app = express()
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
