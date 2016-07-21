d30674b0e89da9fa7e59a87ad8440c5e71bc2403
Zack Michener <zack@zackmichener.net>
2016-07-20 21:38:29 -0700

    clean up index routes

--- routes/index.js
+++ routes/index.js
@@ -2,62 +2,21 @@ var express = require('express');
 var router = express.Router();
 var passport = require('passport');
 var jwt = require('jsonwebtoken');
-// var util = require('../lib/util');
 
-module.exports = function() {
-  // var userCon = require('../lib/UserController')(User, Subs, Post);
+router.get('/', function(req, res) {
+  res.render('app');
+});
 
-  router.get('/', function(req, res) {
-    res.render('app');
-  });
-
-  // router.get('/feed',
-  //   userCon.loginOrContinue,
-  //   userCon.loadLoggedInUser,
-  //   userCon.loadSubscribedPosts,
-  //   setTitle('Feed'),
-  //   util.renderPage('FeedPage'));
-
-  router.post('/login', passport.authenticate('local', {session:false}),
-    function(req, res) {
-      const token = jwt.sign(req.user, req.app.get('apiSecret'), {
-        subject: req.user._id.toString()
-      });
-      
-      res.json({
-        success: true,
-        token
-      });
+router.post('/login', passport.authenticate('local', {session:false}),
+  function(req, res) {
+    const token = jwt.sign(req.user, req.app.get('apiSecret'), {
+      subject: req.user._id.toString()
     });
-
-  // router.get('/login', function(req, res) {
-  //   res.render('page', {
-  //     react: ReactDOM.renderToString(LoginPage())
-  //   });
-  // });
-
-  // router.get('/join', function(req, res) {
-  //   res.render('page', {
-  //     react: ReactDOM.renderToString(SignupPage())
-  //   });
-  // });
-
-  router.get('/logout', function(req, res) {
-    req.logout();
-
-    if (req.accepts('html')) {
-      res.redirect('/');
-    } else {
-      res.end();
-    }
+    
+    res.json({
+      success: true,
+      token
+    });
   });
 
-  // function setTitle(title) {
-  //   return function(req, res, next) {
-  //     req.data.title = title;
-  //     next();
-  //   };
-  // }
-
-  return router;
-};
+module.exports = router;
