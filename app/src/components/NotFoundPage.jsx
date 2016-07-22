b88f042242b5ab998246d9492eeb638014a36bc1
Zack Michener <zack@zackmichener.net>
2016-07-22 15:14:49 -0700

    add back link to 404 page

--- app/src/components/NotFoundPage.jsx
+++ app/src/components/NotFoundPage.jsx
@@ -9,6 +9,7 @@ export default class NotFoundPage extends React.Component {
           <div className='title'>Oh no!</div>
           <div className='subtitle'>We couldn't find what you were looking for.</div>
         </div>
+        <div className='backlink' onClick={this.props.history.goBack}>Go back</div>
         <div className='graphic'>
           <img className='icon' src='/images/astronaut.png' />
         </div>
