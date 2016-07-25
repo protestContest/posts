4047e489989ec7c00be4d69999e6c1b963961ac5
Zack Michener <zack@zackmichener.net>
2016-07-25 15:17:23 -0700

    add update post route on server

--- routes/api.js
+++ routes/api.js
@@ -21,13 +21,38 @@ router.post('/posts',
   function(req, res) {
     if (!req.body.post) return res.status(400).end();
     
-    const post = new Post(req.body.post);
-
-    post.save().then(() => {
-      res.json({post});
-    }).catch((error) => {
-      res.status(400).json({ error });
+    const post = new Post({
+      owner: req.user._id,
+      title: req.body.post.title,
+      body: req.body.post.body
     });
+
+    post.save().then(() => res.json({post}))
+      .catch((error) => res.status(500).json({ error }));
+  });
+
+router.put('/posts/:slug',
+  passport.authenticate('jwt', {session: false}),
+  function(req, res) {
+    if (req.body.post._id !== req.user._id.toString()) return res.status(403).end();
+    if (!req.body.post) return res.status(400).end();
+
+    Post.findOne({ slug: req.params.slug })
+      .then((post) => {
+        if (!post) return res.status(404).end();
+
+        post.title = req.body.post.title || post.title;
+        post.body = req.body.post.body || post.body;
+        post.updated = Date.now();
+
+        if (post.isPrivate && req.body.post.isPrivate === false) {
+          post.isPrivate = false;
+          post.published = Date.now();
+        }
+
+        post.save().then(() => res.json({post}))
+          .catch((error) => res.status(500).json({ error }));
+      });
   });
 
 module.exports = router;
